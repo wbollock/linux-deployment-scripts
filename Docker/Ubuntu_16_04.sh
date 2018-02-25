c628d43e77a52f3a22ce19c5b8db3f344d91f662
Mike <9483554+mb243@users.noreply.github.com>
2018-02-25 12:39:01 -0500

    Add docker-compose

--- Docker/Ubuntu_16_04.sh
+++ Docker/Ubuntu_16_04.sh
@@ -37,7 +37,7 @@ apt-get install -y apt-transport-https ca-certificates curl software-properties-
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-get update
-apt-get install -y docker-ce
+apt-get install -y docker-ce docker-compose
 systemctl enable docker
 systemctl start docker
 
