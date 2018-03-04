5b65fd68de543c9d9b069d01c14e6ce6ba858b83
Mike <9483554+mb243@users.noreply.github.com>
2018-03-04 10:07:51 -0500

    install -y docker-ce

--- Docker/CentOS_7.sh
+++ Docker/CentOS_7.sh
@@ -59,7 +59,7 @@ yum remove -y docker docker-client docker-client-latest docker-common docker-lat
 # Install Docker
 yum install -y yum-utils device-mapper-persistent-data lvm2
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-yum install docker-ce
+yum install -y docker-ce
 systemctl enable docker
 systemctl start docker
 
