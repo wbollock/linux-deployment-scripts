307db5a5831d5112cb2627c861888b53d148d1b8
Mike <9483554+mb243@users.noreply.github.com>
2018-08-04 18:58:33 -0400

    Create script.sh

---
+++ Docker/Crest/ComposeFiles/VPN/script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# OpenVPN Server (single client)
+
+# TODO make this StackScript-ified
+
+# UDF vars
+COUNTRY=aa
+PROVINCE=bb
+CITY=cc
+ORG=dd
+EMAIL=ee
+OU=ff
+CN=gg
+CLIENTNAME="test-user"
+
+# do needfuls here
