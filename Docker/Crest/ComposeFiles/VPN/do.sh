0fb315b59fc83bc0e3d4e04617d4f22147987d3c
Mike <9483554+mb243@users.noreply.github.com>
2018-08-04 18:47:33 -0400

    Add comments

--- Docker/Crest/ComposeFiles/VPN/do.sh
+++ Docker/Crest/ComposeFiles/VPN/do.sh
@@ -2,6 +2,7 @@
 
 # based on https://github.com/kylemanna/docker-openvpn
 
+# write out the docker-compose file
 cat >> docker-compose.yml <<__EOF__
 version: '2'
 services:
@@ -17,8 +18,10 @@ services:
      - ./openvpn-data/conf:/etc/openvpn
 __EOF__
 
+# Generate the OpenVPN server config
 docker-compose run --rm openvpn ovpn_genconfig -u udp://vpn.jawns.io
 
+# Write out EasyRSA env vars to use for cert generation
 cat >> ./openvpn-data/conf/ovpn_env.sh <<__EOF__
 declare -x EASYRSA_REQ_COUNTRY=aa
 declare -x EASYRSA_REQ_PROVINCE=bb
@@ -31,10 +34,22 @@ declare -x EASYRSA_DN=org
 declare -x EASYRSA_BATCH=y
 __EOF__
 
+# Init OpenVPN PKI, skip prompting for password
 docker-compose run --rm openvpn ovpn_initpki nopass
+
+# Start OpenVPN server
 docker-compose up -d openvpn
 
+# Set client name to "test-user"
 export CLIENTNAME="test-user"
 
+# Build client keychain
 docker-compose run --rm openvpn easyrsa build-client-full $CLIENTNAME nopass
+
+# Build client config file from keychain
 docker-compose run --rm openvpn ovpn_getclient $CLIENTNAME > $CLIENTNAME.ovpn
+
+# cat the client config file if desired
+# cat $CLIENTNAME.ovpn
+
+# All done.
