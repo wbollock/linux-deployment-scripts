ff482d8e97abca055a07f3c545f927646f15630c
Mike <9483554+mb243@users.noreply.github.com>
2018-08-04 20:18:57 -0400

    Move vars to docker-compose heredoc

--- Docker/Crest/ComposeFiles/VPN/do.sh
+++ Docker/Crest/ComposeFiles/VPN/do.sh
@@ -8,40 +8,35 @@ version: '2'
 services:
   openvpn:
     cap_add:
-     - NET_ADMIN
+    - NET_ADMIN
     image: kylemanna/openvpn
     container_name: openvpn
     ports:
-     - "1194:1194/udp"
+    - "1194:1194/udp"
     restart: always
     volumes:
-     - ./openvpn-data/conf:/etc/openvpn
+    - ./openvpn-data/conf:/etc/openvpn
+    environment:
+    - EASYRSA_REQ_COUNTRY=aa
+    - EASYRSA_REQ_PROVINCE=bb
+    - EASYRSA_REQ_CITY=cc
+    - EASYRSA_REQ_ORG=dd
+    - EASYRSA_REQ_EMAIL=ee
+    - EASYRSA_REQ_OU=ff
+    - EASYRSA_REQ_CN=gg
+    - EASYRSA_DN=org
+    - EASYRSA_BATCH=y
+    - CLIENTNAME="test-user"
 __EOF__
 
 # Generate the OpenVPN server config
 docker-compose run --rm openvpn ovpn_genconfig -u udp://vpn.jawns.io
-
-# Write out EasyRSA env vars to use for cert generation
-cat >> ./openvpn-data/conf/ovpn_env.sh <<__EOF__
-declare -x EASYRSA_REQ_COUNTRY=aa
-declare -x EASYRSA_REQ_PROVINCE=bb
-declare -x EASYRSA_REQ_CITY=cc
-declare -x EASYRSA_REQ_ORG=dd
-declare -x EASYRSA_REQ_EMAIL=ee
-declare -x EASYRSA_REQ_OU=ff
-declare -x EASYRSA_REQ_CN=gg
-declare -x EASYRSA_DN=org
-declare -x EASYRSA_BATCH=y
-__EOF__
 
 # Init OpenVPN PKI, skip prompting for password
 docker-compose run --rm openvpn ovpn_initpki nopass
 
 # Start OpenVPN server
 docker-compose up -d openvpn
-
-# Set client name to "test-user"
-export CLIENTNAME="test-user"
 
 # Build client keychain
 docker-compose run --rm openvpn easyrsa build-client-full $CLIENTNAME nopass
