341e49b7f70e418366b7236a5f2742fdfabfa94d
Mike <9483554+mb243@users.noreply.github.com>
2018-08-08 12:08:59 -0400

    Append key, because reasons

--- Docker/Ubuntu_16_04.sh
+++ Docker/Ubuntu_16_04.sh
@@ -10,7 +10,7 @@ DEBIAN_FRONTEND=noninteractive apt-get -y upgrade
 # set up ssh pubkey
 echo Setting up ssh pubkey...
 mkdir -p /root/.ssh
-echo "$PUBKEY" > /root/.ssh/authorized_keys
+echo "$PUBKEY" >> /root/.ssh/authorized_keys
 chmod -R 700 /root/.ssh
 echo ...done
 
