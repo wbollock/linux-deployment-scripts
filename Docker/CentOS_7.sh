8a8d65360299e02f748acf25167c8f27de97c374
Mike <9483554+mb243@users.noreply.github.com>
2018-08-08 12:08:48 -0400

    Append key, because reasons

--- Docker/CentOS_7.sh
+++ Docker/CentOS_7.sh
@@ -5,7 +5,7 @@
 # set up ssh pubkey
 echo Setting up ssh pubkey...
 mkdir -p /root/.ssh
-echo "$PUBKEY" > /root/.ssh/authorized_keys
+echo "$PUBKEY" >> /root/.ssh/authorized_keys
 chmod -R 700 /root/.ssh
 echo ...done
 
